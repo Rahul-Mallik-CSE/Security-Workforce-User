13cb79194c5252fe5a92d947c40702be041edbc4
RAHUL MALLIK <rmallik191242@bscse.uiu.ac.bd>
2025-11-11 17:07:44 +0600

    Add SupportPage and SupportForm components for user support functionality

--- src/app/support/page.tsx
+++ src/app/support/page.tsx
@@ -1,9 +1,20 @@
 /** @format */
 
 import React from "react";
+import SupportForm from "@/components/SupportComponents/SupportForm";
 
 const SupportPage = () => {
-  return <div>SupportPage</div>;
+  return (
+    <div className="min-h-screen py-6  ">
+      <div className="max-w-4xl">
+        <h1 className="text-2xl md:text-3xl font-semibold mb-6">Support</h1>
+
+        <div className="">
+          <SupportForm />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SupportPage;
