e7950ee749136edd75f523fedc2d95d4fffd37c4
RAHUL MALLIK <rmallik191242@bscse.uiu.ac.bd>
2025-11-16 12:24:34 +0600

    Add SettingsPage and SettingTabs component for user settings management

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,5 +1,6 @@
 /** @format */
 
+import { SettingTabs } from "@/components/SettingsComponents/SettingTabs";
 import React from "react";
 
 const SettingsPage = () => {
@@ -7,6 +8,9 @@ const SettingsPage = () => {
     <div className=" py-6 ">
       <div className="max-w-[2000px] mx-auto">
         <h1 className="text-2xl md:text-3xl font-semibold mb-6">Settings</h1>
+      </div>
+      <div className="bg-white rounded-xl">
+        <SettingTabs />
       </div>
     </div>
   );
